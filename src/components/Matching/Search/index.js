76d7d7cd3ac5ca171e122def0e03beb868968d13
Mikaner <c0118259dc@edu.teu.ac.jp>
2019-10-07 00:31:08 +0900

    Update Search/index.js

--- src/components/Matching/Search/index.js
+++ src/components/Matching/Search/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { withFirebase } from '../../Firebase';
+import withUserInfo from '../../Auth/Session/withUserInfo';
 import { compose } from 'recompose';
 
 class Search extends React.Component {
+    const user_id = this.props.user_info;
     construnctor(props) {
         super(props);
+        this.componentWillMount.bind(this);
 
+        this.componentDidMount.bind(this);
+    }
+
+    componentWillMount.bind() {
+        const user_set = this.props.firebase.store
+            .collection('users')
+            .doc(this.props.userID)
+            .collection('have_want_set');
+    }
+
+    componentDidMount() {
+        
     }
 
     render() {
-        return ();
+        return (
+            
+        );
     }
 }
 
-export default compose(withFirebase)(Search);
+export default compose(withFirebase, withUserInfo)(Search);
