c7f85eaaed3dd1202c4a5eba90568276336f111d
Mikaner <c0118259dc@edu.teu.ac.jp>
2019-10-06 22:48:24 +0900

    Made Matching branch

---
+++ src/components/Matching/Search/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withFirebase } from '../../Firebase';
+import { compose } from 'recompose';
+
+class Search extends React.Component {
+    construnctor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return ();
+    }
+}
+
+export default compose(withFirebase)(Search);
