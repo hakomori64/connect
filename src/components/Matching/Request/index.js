18220791ac9d392b2231084d8b57478a3f2f76d6
cl0wn <hakomori64@gmail.com>
2019-10-15 12:50:28 +0900

    make sure to load user's have-want-sets

--- src/components/Matching/Request/index.js
+++ src/components/Matching/Request/index.js
@@ -1,45 +1,56 @@
 import React from 'react';
 import withAuthorization from '../../Auth/Session/withAuthorization';
+import styled from 'styled-components';
 
+const PopUp = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    background-color: rgba(0,0,0, 0.5);
+`
+
+const PopupInner = styled.div`
+    position: absolute;
+    left: 25%;
+    right: 25%;
+    top: 25%;
+    bottom: 25%;
+    margin: auto;
+    border-radius: 20px;
+    background: white;
+`
 
 /**
  * This component should be used as below
- * <Request user_id={user_id} />>
+ * <Request user_id={user_id} closePopup={this.closePopup.bind(this)}/>>
  */
 class Request extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            user_info: null,
-        };
+        this.state = {};
     }
 
     componentDidMount() {
-        this.props.firebase.getUserInfo(
-            user_info => this.setState({user_info}),
-            () => this.setState({user_info: null}),
-        )(this.props.user_id);
+
     }
 
     render() {
-        let user_info = null;
-        if (this.state.user_info) {
-            user_info = (
-                <div>
-                    <img src={this.state.user_info.icon_url} alt={this.state.user_info.username} width="200px" height="200px" />
-                    <div>user_name: {this.state.user_info.username}</div>
-                    <div>email: {this.state.user_info.email}</div>
-                    <div>description: </div>
-                    <div>{this.state.user_info.description}</div>
-                </div>
-            )
-        } else {
-            user_info = (
-                <div>Loading...</div>
-            )
-        }
         return (
-            <div>{user_info}</div>
+            <PopUp>
+                <PopupInner>
+                    <img src={this.props.user_info.icon_url} alt={this.props.user_info.username} width="200px" height="200px" />
+                    <div>user_name: {this.props.user_info.username}</div>
+                    <div>email: {this.props.user_info.email}</div>
+                    <div>description: </div>
+                    <div>{this.props.user_info.description}</div>
+                    <button onClick={this.props.closePopup}>close popup</button>
+                </PopupInner>
+            </PopUp>
         )
     }
 }
