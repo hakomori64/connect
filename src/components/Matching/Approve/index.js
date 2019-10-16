3f214526d8e4c79ea516c89fa4f8e62ae000a63c
cl0wn <hakomori64@gmail.com>
2019-10-16 22:33:23 +0900

    approve work

--- src/components/Matching/Approve/index.js
+++ src/components/Matching/Approve/index.js
@@ -46,11 +46,33 @@ class Approve extends React.Component {
         return true;
     }
 
+    handleApproval = request_info => {
+        this.props.firebase.store.collection('requests').doc(request_info.id).delete();
+        this.props.firebase.store.collection('rooms').add({
+            name: request_info.room_name,
+            members: [this.props.authUser.userID, request_info.from.userID],
+        }).then(room_ref => {
+            [this.props.authUser.userID, request_info.from.userID].forEach(user_id => {
+                const user_ref = this.props.firebase.store.collection('users').doc(user_id);
+                this.props.firebase.store.runTransaction(transaction => {
+                    return transaction.get(user_ref).then(user_doc => {
+                        if (!user_doc.exists) return;
+                        const user_room_ids = user_doc.data().room_ids;
+                        user_room_ids.push(room_ref.id);
+                        transaction.update(user_ref, {room_ids: user_room_ids});
+                    });
+                }).catch(error => {
+                    console.log("Transaction failed: ", error);
+                });
+            });
+        });
+    }
+
     render() {
         let request_list = this.state.no_requests ? <div>No Requests</div> : <div>Now Loading...</div>;
         if (this.state.requests_info.length) {
             request_list = this.state.requests_info.map(request_info => (
-                <li key={request_info.id}>{request_info.from.username}さんあなたに友達リクエストを送信しました。</li>
+                <div key={request_info.id}>{request_info.from.username}さんあなたに友達リクエストを送信しました。<button onClick={event => this.handleApproval(request_info)}>承認</button></div>
             ))
             console.log(request_list);
         }
